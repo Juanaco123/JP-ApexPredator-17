8ce4828466b25f7beba87ff2cf18e983dfd44031
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-04-27 09:11:06 -0500

    Create apex predator model, create decoder.

---
+++ JPApexPredator17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredator17
+//
+//  Created by Juan Camilo Victoria Pacheco on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredator17/JPApexPredator17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredator17App.swift
+//  JPApexPredator17
+//
+//  Created by Juan Camilo Victoria Pacheco on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredator17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
