0de5fbc9abfc79ab849b552a3009b63b3913710e
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-06-03 15:04:02 -0500

    Add gradient to Predator Detail screen.

--- JPApexPredator17/PredatorDetail.swift
+++ JPApexPredator17/PredatorDetail.swift
@@ -19,6 +19,17 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(
+                                stops: [
+                                    Gradient.Stop(color: .clear, location: 0.8),
+                                    Gradient.Stop(color: .black, location: 1)
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        }
+                    
                     // Dino image
                     Image(predator.image)
                         .resizable()
@@ -77,6 +88,6 @@ struct PredatorDetail: View {
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().apexPredators[2])
-        //.preferredColorScheme(.dark)
+    PredatorDetail(predator: Predators().apexPredators[10])
+        .preferredColorScheme(.dark)
 }
